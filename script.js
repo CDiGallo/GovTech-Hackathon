9681229ab63cfddbc59b7dbf3ea50a7e7c9b53d7
Stefan Mueller <mullzk@users.noreply.github.com>
2023-03-24 12:17:23 +0100

    Add breadcrump, hardcoded

--- script.js
+++ script.js
@@ -17,20 +17,70 @@ function initApplication() {
 		visualizedata(rootNode);
 		listNode(rootNode);
 		listChildren(rootNode);
+		console.log(rootNode);
 	});
 }
 
 
+async function fetchDetailsForNode(nodeid) {
+	let queryBody = `
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+SELECT * WHERE { 
+	?child <https://www.ica.org/standards/RiC/ontology#isOrWasIncludedIn> <https://culture.ld.admin.ch/ais/` + nodeid + `>. 
+	?child <https://www.ica.org/standards/RiC/ontology#title> ?child_title. 
+	?child <https://www.ica.org/standards/RiC/ontology#hasRecordSetType> ?child_recordSetType. 
+	?child <https://www.ica.org/standards/RiC/ontology#isAssociatedWithDate> ?date. 
+	?date <https://www.ica.org/standards/RiC/ontology#beginningDate> ?beginningDate. 
+	?date <https://www.ica.org/standards/RiC/ontology#endDate> ?endDate. 
+	?child <https://schema.ld.admin.ch/referenceCode> ?childReferenceCode. 
+	?child <https://schema.ld.admin.ch/position> ?position. 
+} 
+ORDER BY ASC(?position) 
+LIMIT 100
+`;
+
+	var details = {
+		'query': queryBody,
+		'lang': 'de'
+	};
+
+	var formBody = [];
+		for (var property in details) {
+		var encodedKey = encodeURIComponent(property);
+		var encodedValue = encodeURIComponent(details[property]);
+		formBody.push(encodedKey + "=" + encodedValue);
+	}
+	formBody = formBody.join("&");
+
+	let response = await fetch(
+		"https://lindas.admin.ch/query",
+		{
+			method: 'POST',
+			headers: {
+				'accept' : "application/sparql-results+json,*/*;q=0.9",
+				'content-type' : "application/x-www-form-urlencoded"
+			},
+			body: formBody
+		}
+	);
+
+	let responsejson = await response.json(); 
+}
+
 
 async function fetchNode(nodeid, node, level) {
-	let queryBody = "";
-	queryBody += "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ";
-	queryBody += "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ";
-	queryBody += "SELECT * WHERE { ";
-	queryBody += "?child <https://www.ica.org/standards/RiC/ontology#isOrWasIncludedIn> <https://culture.ld.admin.ch/ais/" + nodeid + ">. ";
-	queryBody += "?child <https://www.ica.org/standards/RiC/ontology#title> ?child_title. ";
-    queryBody += "?child <https://schema.ld.admin.ch/position> ?position. ";
-	queryBody += "} ORDER BY ASC(?position) LIMIT 100";
+	let queryBody = `
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+SELECT * WHERE { 
+	?child <https://www.ica.org/standards/RiC/ontology#isOrWasIncludedIn> <https://culture.ld.admin.ch/ais/` + nodeid + `>. 
+	?child <https://www.ica.org/standards/RiC/ontology#title> ?child_title. 
+	?child <https://schema.ld.admin.ch/position> ?position. 
+} 
+ORDER BY ASC(?position) 
+LIMIT 100
+`;
 
 	var details = {
 		'query': queryBody,
@@ -39,9 +89,9 @@ async function fetchNode(nodeid, node, level) {
 	
 	var formBody = [];
 	for (var property in details) {
-	var encodedKey = encodeURIComponent(property);
-	var encodedValue = encodeURIComponent(details[property]);
-	formBody.push(encodedKey + "=" + encodedValue);
+		var encodedKey = encodeURIComponent(property);
+		var encodedValue = encodeURIComponent(details[property]);
+		formBody.push(encodedKey + "=" + encodedValue);
 	}
 	formBody = formBody.join("&");
 
@@ -59,8 +109,6 @@ async function fetchNode(nodeid, node, level) {
 
 	let responsejson = await response.json(); 
 	let childs = responsejson['results']['bindings']
-	node.identifier = node.value;
-	node.value = 1;
 
 	await fetchChildren(childs, node, level);
 }
@@ -74,18 +122,18 @@ async function fetchChildren(children, parentNode, level) {
 	for (const child of children) {
 
 		let childId = child.child.value.replace("https://culture.ld.admin.ch/ais/", "");
+		console.log(child);
 
 		let childNode = {
 			"name": child.child_title.value,
-			"value": childId,
+			"value": 1,
+			"identifier": childId,
 			"children" : []
 		}
 
 		if (level > 0) {
 			fetchCalls.push(fetchNode(childId, childNode, level - 1));
 		}
-		childNode.identifier = childNode.value;
-		childNode.value = 1;
 	
 		parentNode.children.push(childNode);
 	}
@@ -113,3 +161,4 @@ function listNode(node) {
 function listChildren(node) {
 	
 }
+
